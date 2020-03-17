35da19698d65c83687fec7661ae2f3a749ac09f6
Kevin Verschoor <k.verschoor@pay.nl>
2020-03-17 10:15:42 +0100

    Add getters for Status

--- src/Result/Status/Status.php
+++ src/Result/Status/Status.php
@@ -15,4 +15,74 @@ class Status extends Result
     {
         return $this->data['data'];
     }  
+
+    public function getState()
+    {
+        return $this->data['data']['state'];
+    }  
+
+    public function getStatusMessage()
+    {
+        return $this->data['data']['statusMessage'];
+    }  
+
+    public function getReference()
+    {
+        return $this->data['data']['reference'];
+    }
+
+    public function getId()
+    {
+        return (isset($this->data['data']['id'])) ? $this->data['data']['id'] : '';
+    }
+
+    public function getName()
+    {
+        return (isset($this->data['data']['name'])) ? $this->data['data']['name'] : array();
+    }    
+
+    public function getPrefLastName()
+    {
+        return (isset($this->data['data']['name']['prefLastName'])) ? $this->data['data']['name']['prefLastName'] : '';
+    }    
+
+    public function getPrefLastNamePrefix()
+    {
+        return (isset($this->data['data']['name']['prefLastNamePrefix'])) ? $this->data['data']['name']['prefLastNamePrefix'] : '';
+    } 
+
+    public function getLegalLastName()
+    {
+        return (isset($this->data['data']['name']['legalLastName'])) ? $this->data['data']['name']['legalLastName'] : '';
+    } 
+
+    public function getLegalLastNamePrefix()
+    {
+        return (isset($this->data['data']['name']['legalLastNamePrefix'])) ? $this->data['data']['name']['legalLastNamePrefix'] : '';
+    } 
+
+    public function getPartnerLastName()
+    {
+        return (isset($this->data['data']['name']['partnerLastName'])) ? $this->data['data']['name']['partnerLastName'] : '';
+    } 
+
+    public function getPartnerLastNamePrefix()
+    {
+        return (isset($this->data['data']['name']['partnerLastNamePrefix'])) ? $this->data['data']['name']['partnerLastNamePrefix'] : '';
+    } 
+
+    public function getInitials()
+    {
+        return (isset($this->data['data']['name']['initials'])) ? $this->data['data']['name']['initials'] : '';
+    } 
+
+    public function getFirstName()
+    {
+        return (isset($this->data['data']['name']['firstName'])) ? $this->data['data']['name']['firstName'] : '';
+    } 
+
+    public function getDateOfBirth()
+    {
+        return (isset($this->data['data']['dateOfBirth'])) ? $this->data['data']['dateOfBirth'] : '';
+    }
 }
