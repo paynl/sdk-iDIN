6b54ad8fdc252dcabe54b91e2efad94f5cce13b9
Maurice Villhaber <maurice@pay.nl>
2020-03-18 19:54:38 +0100

    Update Status.php
    Adds isEighteenOrOlder() method to the result of the status request.

--- src/Result/Status/Status.php
+++ src/Result/Status/Status.php
@@ -14,17 +14,17 @@ class Status extends Result
     public function getData()
     {
         return $this->data['data'];
-    }  
+    }
 
-    public function getState()
+    public function getStatus()
     {
         return $this->data['data']['state'];
-    }  
+    }
 
     public function getStatusMessage()
     {
         return $this->data['data']['statusMessage'];
-    }  
+    }
 
     public function getReference()
     {
@@ -39,50 +39,55 @@ class Status extends Result
     public function getName()
     {
         return (isset($this->data['data']['name'])) ? $this->data['data']['name'] : array();
-    }    
+    }
 
     public function getPrefLastName()
     {
         return (isset($this->data['data']['name']['prefLastName'])) ? $this->data['data']['name']['prefLastName'] : '';
-    }    
+    }
 
     public function getPrefLastNamePrefix()
     {
         return (isset($this->data['data']['name']['prefLastNamePrefix'])) ? $this->data['data']['name']['prefLastNamePrefix'] : '';
-    } 
+    }
 
     public function getLegalLastName()
     {
         return (isset($this->data['data']['name']['legalLastName'])) ? $this->data['data']['name']['legalLastName'] : '';
-    } 
+    }
 
     public function getLegalLastNamePrefix()
     {
         return (isset($this->data['data']['name']['legalLastNamePrefix'])) ? $this->data['data']['name']['legalLastNamePrefix'] : '';
-    } 
+    }
 
     public function getPartnerLastName()
     {
         return (isset($this->data['data']['name']['partnerLastName'])) ? $this->data['data']['name']['partnerLastName'] : '';
-    } 
+    }
 
     public function getPartnerLastNamePrefix()
     {
         return (isset($this->data['data']['name']['partnerLastNamePrefix'])) ? $this->data['data']['name']['partnerLastNamePrefix'] : '';
-    } 
+    }
 
     public function getInitials()
     {
         return (isset($this->data['data']['name']['initials'])) ? $this->data['data']['name']['initials'] : '';
-    } 
+    }
 
     public function getFirstName()
     {
         return (isset($this->data['data']['name']['firstName'])) ? $this->data['data']['name']['firstName'] : '';
-    } 
+    }
 
     public function getDateOfBirth()
     {
         return (isset($this->data['data']['dateOfBirth'])) ? $this->data['data']['dateOfBirth'] : '';
+    }
+
+    public function isEighteenOrOlder()
+    {
+        return (isset($this->data['data']['isEighteen'])) ? filter_var($this->data['data']['isEighteen'], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) : NULL;
     }
 }
